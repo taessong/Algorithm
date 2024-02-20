85bb9fe6d81f2e4b96b73c5b83b1e7a37c8c5b6c
kts <156993058+taessong@users.noreply.github.com>
2024-02-20 17:44:11 +0900

    Update 방 배정.java

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -13,14 +13,16 @@ public class Main {
     		
     		int[][] student = new int[7][2];
     		int room = 0;
-    		
+
+            // student 배열에 grade와 gender에 맞는 위치에 ++ 해주기
     		for(int i=0; i<N; i++) {
     			st = new StringTokenizer(br.readLine());
     			int gender = Integer.parseInt(st.nextToken());
     			int grade = Integer.parseInt(st.nextToken());
     			student[grade][gender]++;
     		}
-    		
+
+            // 만약 나머지가 0이면 student[i][j] / K, 0이 아니면 (student[i][j] / K) + 1으로 방의 개수를 구한다.
     		for(int i=1; i<7; i++) {
     			for(int j=0; j<2; j++) {
     				if(student[i][j] % K == 0) {
