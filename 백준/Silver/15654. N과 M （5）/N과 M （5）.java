3fcf65aace958376e47c2349538b8555d9dde3e4
kts <156993058+taessong@users.noreply.github.com>
2024-03-24 18:05:36 +0900

    [Silver III] Title: N과 M (5), Time: 412 ms, Memory: 41980 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] result, arr, visited;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+		visited = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		perm(0);
+		System.out.println(sb);
+	}
+	
+	public static void perm(int count) {
+		if(count == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(result[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for(int i=0; i<N; i++) {
+			if(visited[i] == 1) continue;
+			else{
+				visited[i] = 1;
+				result[count] = arr[i];
+				perm(count+1);
+				visited[i] = 0;
+			}
+		}
+	}
+}
