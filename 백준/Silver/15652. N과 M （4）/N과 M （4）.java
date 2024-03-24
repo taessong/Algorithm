062d8264b653945c8fd5ea3fe98e5707f87ea927
kts <156993058+taessong@users.noreply.github.com>
2024-03-24 17:35:38 +0900

    [Silver III] Title: N과 M (4), Time: 208 ms, Memory: 20276 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		result = new int[M];
+		
+		perm(0, 1);
+		System.out.println(sb);
+	}
+	
+	public static void perm(int count, int start) {
+		if(count == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(result[i]+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start; i<=N; i++) {
+			result[count] = i;
+			perm(count+1, start);
+			start++;
+		}
+	}
+}
