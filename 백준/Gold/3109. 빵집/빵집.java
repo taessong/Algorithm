f8c6595f4556304e2313d7b59bddfeb5812f2bc6
kts <156993058+taessong@users.noreply.github.com>
2024-04-10 16:42:42 +0900

    Update 빵집.java

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -7,7 +7,7 @@ public class Main {
     static int R, C, result;
     static char[][] map;
     static boolean[][] visited;
-    static boolean[] started;
+    static boolean[] started; // 하나의 출발지에는 하나의 파이프만 연결되어야 하기에 생성
     static int[] dr = {-1, 0, 1}, dc = {1, 1, 1}; // 오른쪽 위, 오른쪽, 오른쪽 아래
     
     public static void main(String[] args) throws IOException{
@@ -40,14 +40,14 @@ public class Main {
 		
 		if(c == C-1) {
 			result++;
-			started[start] = true;
+			started[start] = true; // 해당 start에서 시작한 파이프는 생성되었으니 이제 생성하면 안 됨!
 			return;
 		}
 		
 		for(int d=0; d<3; d++) {
 			int nr = r + dr[d], nc = c+ dc[d];
 			if(nr >= 0 && nr < R && nc >= 0 && nc < C && !visited[nr][nc] && map[nr][nc] == '.') {
-				if(started[start]) return;
+				if(started[start]) return; // 만약 하나의 시작점에서 출발해, 끝까지 연결된 파이프가 이미 있다면 return
 
 				visited[nr][nc] = true;
 				pipe(start, nr, nc);
