673953c608dff5b2f8be453b1517d59008cf9716
Taeseong Kim <156993058+taessong@users.noreply.github.com>
2025-01-09 22:43:51 +0900

    Update 수열의 합

--- 백준/Silver/1024. 수열의 합/수열의 합.java
+++ 백준/Silver/1024. 수열의 합/수열의 합.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ_1024 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,17 +12,18 @@ public class Main {
         int L = Integer.parseInt(st.nextToken());
 
         while(true) {
-            int min = N / L - ((L - 1) / 2);
-            if(min < 0 || L > 100) {
+            // 등차수열의 합 공식을 이용해서 구한 첫째항
+            int a = N / L - ((L - 1) / 2);
+            if(a < 0 || L > 100) {
                 System.out.println(-1);
                 break;
             }
 
             // 등차수열의 합 공식
-            int sum = L * (2 * min + (L - 1)) / 2;
+            int sum = L * (2 * a + (L - 1)) / 2;
             if(sum == N) {
                 for(int i = 0; i < L; i++) {
-                    System.out.print(min + i + " ");
+                    System.out.print(a + i + " ");
                 }
                 break;
             }
