610722e460d8f17e5761d055ea7c2466d18abdef
Taeseong Kim <156993058+taessong@users.noreply.github.com>
2025-09-08 17:08:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1532 ms, Memory: 337144 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int[] arr = new int[10001];
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			arr[Integer.parseInt(br.readLine())]++;
+		}
+		
+		for(int i = 0; i < 10001; i++) {
+			while(arr[i] > 0) {
+				sb.append(i).append("\n");
+				arr[i]--;
+			}
+		}
+		
+		System.out.println(sb);
+	}
+}
